cc7213eb45bbe97b09b90bf00e95bc185a7f7cd6
Flesner96 <164926647+Flesner96@users.noreply.github.com>
2024-06-07 20:22:41 +0200

    add accounts app and urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for ProjektKoncowy_Progess_tracker project.
+URL configuration for KsiegarniaS18 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -14,10 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+
+from django.urls import path
+from accounts import views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("accounts/", include("accounts.urls")),
+    path('register/', views.CreateUserView.as_view(), name="register"),
+    path('login/', views.LoginView.as_view(), name="login"),
+    path('logout/', views.LogoutView.as_view(), name="logout"),
 ]
